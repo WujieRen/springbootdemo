095dfbd5159b35464b5bb22c41abf8710a7ab9d4
WujieRen <rwj005@outlook.com>
2017-12-27 12:50:44 +0800

    springbooe demo1

---
+++ src/main/java/com/springboot/demo1/HelloController.java
@@ -0,0 +1,17 @@
+package com.springboot.demo1;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by renwujie on 2017/12/27 at 11:05
+ *
+ * HelloWorldTest
+ */
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello SpringBoot!";
+    }
+}
