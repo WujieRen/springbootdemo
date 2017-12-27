b86c74e953748caf02e045543ff81100f1daaebc
WujieRen <rwj005@outlook.com>
2017-12-27 14:52:58 +0800

    测试Fetch成功

--- src/main/java/com/springboot/demo1/TestGitFetch.java
+++ src/main/java/com/springboot/demo1/TestGitFetch.java
@@ -4,4 +4,7 @@ package com.springboot.demo1;
  * Created by renwujie on 2017/12/27 at 14:45
  */
 public class TestGitFetch {
+    public static void main(String[] args){
+        System.out.println("TestFetchSuccess!!!");
+    }
 }
