6e862369aa84e4ae88976f360d4518cf326cc193
WujieRen <rwj005@outlook.com>
2017-12-27 15:17:51 +0800

    测试New branch成功

---
+++ src/main/java/com/springboot/demo1/TestBranch.java
@@ -0,0 +1,10 @@
+package com.springboot.demo1;
+
+/**
+ * Created by renwujie on 2017/12/27 at 15:08
+ */
+public class TestBranch {
+    public static void main(String[] args){
+        System.out.println("add a new branch");
+    }
+}
