2bc52645a947fcd0d793b231b5e98f9b7439e5ba
WujieRen <rwj005@outlook.com>
2017-12-28 11:15:07 +0800

    删除testGitAdd

--- src/main/java/com/springboot/demo1/HelloController.java
+++ src/main/java/com/springboot/demo1/HelloController.java
@@ -1,7 +1,13 @@
 package com.springboot.demo1;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.List;
 
 /**
  * Created by renwujie on 2017/12/27 at 11:05
@@ -9,9 +15,30 @@ import org.springframework.web.bind.annotation.RestController;
  * HelloWorldTest
  */
 @RestController
+@RequestMapping("/hello")
 public class HelloController {
-    @RequestMapping("/hello")
+
+    @RequestMapping
     public String hello(){
         return "hello SpringBoot!";
+    }
+
+    @RequestMapping("/info")
+    public Map<String, String> getInfo(@RequestParam String name) {
+        Map<String, String> map = new HashMap<>();
+        map.put("name", name);
+        return map;
+    }
+
+    @RequestMapping("/list")
+    public List<Map<String, String>> getList() {
+        List<Map<String, String>> list = new ArrayList<Map<String, String>>();
+        Map<String, String> map = null;
+        for (int i = 1; i <= 5; i++) {
+            map = new HashMap<>();
+            map.put("name", "Shanhy-" + i);
+            list.add(map);
+        }
+        return list;
     }
 }
---
+++ src/main/webApp/WEB-INF/jsp/page/page1.jsp
@@ -0,0 +1,19 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: renwujie
+  Date: 2017/12/28 0028
+  Time: 11:05
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+    <title>Spring Boot Sample</title>
+</head>
+
+<body>
+<h1>${content }</h1>
+<h1>${selfTest }</h1>
+</body>
+</html>
