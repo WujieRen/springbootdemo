c3599c704797d7835fbf47f67f47598cc4834593
WujieRen <rwj005@outlook.com>
2017-12-28 16:55:33 +0800

    SpringBoot Interceptor demo

---
+++ src/main/java/com/springboot/intercepter/IntercepterSampleApplication.java
@@ -0,0 +1,14 @@
+package com.springboot.intercepter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by renwujie on 2017/12/28 at 16:51
+ */
+@SpringBootApplication
+public class IntercepterSampleApplication {
+    public static void main(String[] args){
+        SpringApplication.run(IntercepterSampleApplication.class, args);
+    }
+}
---
+++ src/main/java/com/springboot/intercepter/MyWebAppConfigurer.java
@@ -0,0 +1,22 @@
+package com.springboot.intercepter;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by renwujie on 2017/12/28 at 16:37
+ */
+@Configuration
+public class MyWebAppConfigurer extends WebMvcConfigurerAdapter {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        // 多个拦截器组成一个拦截器链
+        // addPathPatterns 用于添加拦截规则
+        // excludePathPatterns 用户排除拦截
+        registry.addInterceptor(new MyInterceptor1()).addPathPatterns("/*");
+        registry.addInterceptor(new MyIntercepter2()).addPathPatterns("/**");
+
+        super.addInterceptors(registry);
+    }
+}
