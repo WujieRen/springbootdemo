51d11bc78d8e50dcb9a29474f9d5146e46da73cb
WujieRen <rwj005@outlook.com>
2018-02-07 18:34:50 +0800

    lemonMockData jifei_kylin

---
+++ src/main/java/com/lemon/jifei_kylin/FKJiFeiKylin.java
@@ -0,0 +1,12 @@
+package com.lemon.jifei_kylin;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by renwujie on 2018/02/06 at 18:23
+ */
+@Component
+public class FKJiFeiKylin {
+    private String encoding = "UTF-8";
+
+}
---
+++ src/main/java/com/lemon/t.java
@@ -0,0 +1,7 @@
+package com.lemon;
+
+/**
+ * Created by renwujie on 2018/02/06 at 18:22
+ */
+public class t {
+}
